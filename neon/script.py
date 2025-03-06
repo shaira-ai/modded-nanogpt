a9517ba4294414f68aea3d2b89e982f4d01f2688
sharpobject <sharpobject@gmail.com>
2025-03-06 12:56:52 -0800

    custom loss (does the same thing as the normal loss)

--- neon/script.py
+++ neon/script.py
@@ -1,7 +1,41 @@
 import torch
 import math
 import sys
+import transformers
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+def ForCausalLMLoss(
+    logits, labels, vocab_size: int, num_items_in_batch: int = None, ignore_index: int = -100, **kwargs
+):
+    # Upcast to float if we need to compute the loss to avoid potential precision issues
+    logits = logits.float()
+    labels = labels.to(logits.device)
+    # Shift so that tokens < n predict n
+    labels = torch.nn.functional.pad(labels, (0, 1), value=ignore_index)
+    shift_labels = labels[..., 1:].contiguous()
+
+    # Flatten the tokens
+    logits = logits.view(-1, vocab_size)
+    #print(logits)
+    shift_labels = shift_labels.view(-1)
+    # Enable model parallelism
+    shift_labels = shift_labels.to(logits.device)
+    #print(shift_labels)
+    probabilities = torch.nn.functional.softmax(logits, dim=1)
+    target_probabilities = []
+    for idx,x in enumerate(shift_labels):
+        if x == -100:
+            continue
+        target_probabilities.append(probabilities[idx][x].item())
+    #print(target_probabilities)
+    log_probs = torch.log(torch.tensor(target_probabilities))
+    #print(log_probs)
+    average_log_prob = log_probs.mean().item()
+    sum_log_prob = log_probs.sum().item()
+    #print(math.exp(-average_log_prob))
+    #print(-sum_log_prob/(44 * math.log(2)))
+    loss = transformers.loss.loss_utils.fixed_cross_entropy(logits, shift_labels, num_items_in_batch, ignore_index, **kwargs)
+    return loss
 
 def calculate_perplexity(file_path):
     try:
@@ -23,6 +57,7 @@ def calculate_perplexity(file_path):
     
     with torch.no_grad():
         tokens_tensor = torch.tensor([tokens])
+        model._loss_function = ForCausalLMLoss
         loss = model(tokens_tensor, labels=tokens_tensor).loss.item()
     
     perplexity = math.exp(loss)  # Perplexity = e^ALLT
